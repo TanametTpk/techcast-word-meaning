09b899f766abcd8166d4a22d60b18c4e7200c191
TanametTpk <40013261+TanametTpk@users.noreply.github.com>
2020-12-24 06:19:34 +0000

    Deploying to gh-pages from  @ f851f2dbc14571ff78c035b7adb0b82aacacec19 🚀

---
+++ _next/static/JStKIklBstK8pUfqMEbGI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-274b4bb4e90572609d27.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-154b2ec68bf9e7204aeb.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
